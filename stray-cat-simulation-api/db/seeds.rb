c0a222efa7c2c69a78c617d5882fa060cabbb0d0
jousethen <jthen93@gmail.com>
2021-09-14 12:32:04 -0400

    Seeds added

--- stray-cat-simulation-api/db/seeds.rb
+++ stray-cat-simulation-api/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Test Seeding
+cats = Cat.create([{name: "Test Kitty 1", affection: 10}, {name: "Test Kitty 2", affection: 5 }])
