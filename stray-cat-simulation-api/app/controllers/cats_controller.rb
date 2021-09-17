5874c055cbcd184b762cb30625877667cb37a652
jousethen <jthen93@gmail.com>
2021-09-17 12:46:53 -0400

    Updating cats is working!

--- stray-cat-simulation-api/app/controllers/cats_controller.rb
+++ stray-cat-simulation-api/app/controllers/cats_controller.rb
@@ -4,7 +4,23 @@ class CatsController < ApplicationController
     render json: cats
   end
 
+ 
+
   def update
-    puts params;
+    cat = Cat.find_by_id(params[:id])
+
+    cat.update(cat_params)
+
+    if cat.save
+      puts "success"
+    else
+      puts "FAiled"
+    end
   end
+
+   private 
+  def cat_params
+    params.require(:cat).permit(:id, :name, :hp, :hunger, :affection)
+  end
+
 end
