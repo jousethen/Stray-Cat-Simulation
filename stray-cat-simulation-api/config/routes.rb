99e8e38ecf62856389d410ea22be60344e735347
jousethen <jthen93@gmail.com>
2021-09-19 13:58:14 -0400

    Created User Table

--- stray-cat-simulation-api/config/routes.rb
+++ stray-cat-simulation-api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get '/cats' => 'cats#index'
   patch '/cats/:id' => 'cats#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
