dc9019b6a92f247374e81f8dc7c11ad5c9d6d46f
jousethen <jthen93@gmail.com>
2021-09-24 13:43:47 -0400

    Added Index to accessoriescontroller

--- stray-cat-simulation-api/app/controllers/accessories_controller.rb
+++ stray-cat-simulation-api/app/controllers/accessories_controller.rb
@@ -1,2 +1,7 @@
 class AccessoriesController < ApplicationController
+    def index
+      accessories = Accessory.all
+      render json: accessories
+    end
+
 end
--- stray-cat-simulation-api/config/routes.rb
+++ stray-cat-simulation-api/config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   get '/user' => 'users#show'
   post '/user' => 'users#carry_over'
+
+  get '/accessories' => 'accessories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end 
