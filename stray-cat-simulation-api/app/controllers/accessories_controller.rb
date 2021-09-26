dc898278478762d2576b59dac3748114d85b951f
jousethen <jthen93@gmail.com>
2021-09-26 17:22:44 -0400

    Created Routing and Controller for Accessory Update

--- stray-cat-simulation-api/app/controllers/accessories_controller.rb
+++ stray-cat-simulation-api/app/controllers/accessories_controller.rb
@@ -4,4 +4,20 @@ class AccessoriesController < ApplicationController
     render json: accessories
   end
 
+  def update
+    accessory = Accessory.find_by_id(params[:id])
+
+    accessory.update(accessory_params)
+    if accessory.save
+      puts "success"
+    else
+      puts "FAiled"
+    end
+  end
+
+  private 
+  def accessory_params
+    params.require(:accessory).permit(:id, :cat_id)
+  end
+
 end
