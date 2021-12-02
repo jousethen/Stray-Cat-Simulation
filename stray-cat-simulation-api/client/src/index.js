dc69fa6a9efcd62b8c27e3fe7417fb2ac9b612fc
jousethen <jthen93@gmail.com>
2021-12-01 22:11:05 -0500

    installed redux and thunk. Also created default route

--- stray-cat-simulation-api/client/src/index.js
+++ stray-cat-simulation-api/client/src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
