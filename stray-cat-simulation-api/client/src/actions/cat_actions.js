eba82c3db5a24965700a5264197b3ae11fa0bc69
jousethen <jthen93@gmail.com>
2021-12-02 14:55:58 -0500

    cat action 'fetctAllCats' complete

--- stray-cat-simulation-api/client/src/actions/cat_actions.js
+++ stray-cat-simulation-api/client/src/actions/cat_actions.js
@@ -1,18 +1,18 @@
 export const fetchAllCats = () => {
-  fetch('/api/cats', {
-    method: 'GET', headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json);
-      let cats = {
-        cat: "cat"
+  return (dispatch) => {
+    dispatch({ type: "LOADING_CATS" });
+    fetch('/api/cats', {
+      method: 'GET', headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       }
-      return cats
-    });
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        dispatch({ type: "LOAD_CATS_SUCCESS", cats: json })
+      });
+  };
 }
