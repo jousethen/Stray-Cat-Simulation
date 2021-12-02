d8fae06275d57a6954a0259195552dd7fdb78ed6
jousethen <jthen93@gmail.com>
2021-12-01 22:31:55 -0500

    Added Api namespace to avoid conflicts with frontend routing

--- stray-cat-simulation-api/config/routes.rb
+++ stray-cat-simulation-api/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :accessories
-  get '/cats' => 'cats#index'
-  patch '/cats/:id' => 'cats#update'
-  post '/cats/overnight-adventures' => 'cats#overnight'
+  namespace :api do
+    resources :accessories
+    get '/cats' => 'cats#index'
+    patch '/cats/:id' => 'cats#update'
+    post '/cats/overnight-adventures' => 'cats#overnight'
 
-  get '/user' => 'users#show'
-  post '/user' => 'users#carry_over'
+    get '/user' => 'users#show'
+    post '/user' => 'users#carry_over'
 
-  get '/accessories' => 'accessories#index'
-  patch '/accessories/:id' => 'accessories#update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end 
+    get '/accessories' => 'accessories#index'
+    patch '/accessories/:id' => 'accessories#update'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end 
+end
