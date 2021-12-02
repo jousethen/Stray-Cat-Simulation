fbda1ffbaf3962bdc163a4747d602538886899d6
jousethen <jthen93@gmail.com>
2021-12-01 22:30:43 -0500

    Base with Redux, Routers, and Thunk configured

--- stray-cat-simulation-api/client/src/index.js
+++ stray-cat-simulation-api/client/src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import thunk from "redux-thunk";
+import reducers from "./reducers/index";
 const store = createStore(reducers, applyMiddleware(thunk));
 
 
