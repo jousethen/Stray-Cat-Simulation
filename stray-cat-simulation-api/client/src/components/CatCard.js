42315c05aea5c4cb52e696830a35f794462dd062
jousethen <jthen93@gmail.com>
2021-12-03 00:55:22 -0500

    Added buttons and meters to cat cards

--- stray-cat-simulation-api/client/src/components/CatCard.js
+++ stray-cat-simulation-api/client/src/components/CatCard.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar'
+import { ProgressBar, Button } from 'react-bootstrap'
 
 const CatCard = (props) => {
   let imageUrl = `images/${props.cat.image}`;
-  console.log(props)
+
   return (
     <div className="cat-card">
       <img
         src={imageUrl}
         alt={props.cat.image} />
       <div className="cat-name">{props.cat.name}</div>
-      <div className="cat-hp">
-        <ProgressBar now={60} />
+      <div className="cat-stat">
+        Health: {props.cat.hp}/10
+        <ProgressBar min="0" max="10" now={props.cat.hp} />
+      </div>
+      <div className="cat-stat">
+        Food: {props.cat.food}/10
+        <ProgressBar min="0" max="10" now={props.cat.hp} />
+      </div>
+      <div className="cat-stat">
+        Affection: {props.cat.affection}/10
+        <ProgressBar min="0" max="10" now={props.cat.hp} />
       </div>
 
+      <Button className="action">Feed</Button>
+      <Button className="action">Heal</Button>
+      <Button className="action">Pet</Button>
     </div>
   )
 }
