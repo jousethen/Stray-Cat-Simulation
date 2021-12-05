3ec6affa5b8e40ee5d73bb23feae113fe030a2db
jousethen <jthen93@gmail.com>
2021-12-05 17:11:08 -0500

    Accessory reducer added

---
+++ stray-cat-simulation-api/client/src/reducers/accessory_reducer.js
@@ -0,0 +1,15 @@
+export const accessoryReducer = (state = { accessories =[] }, action) => {
+  switch (action.type) {
+    case "LOADING_ACCESSORIES":
+      return {
+        ...state,
+      };
+    case "LOAD_ACCESSORIES_SUCCESS":
+      return {
+        ...state,
+        actions: action.accessories
+      }
+    default:
+      return state;
+  }
+};
--- stray-cat-simulation-api/client/src/reducers/index.js
+++ stray-cat-simulation-api/client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { catReducer } from './cat_reducer';
 import { userReducer } from './user_reducer';
+import { accessoryReducer } from './accessory_reducer';
 
 export default combineReducers({
 
   cats: catReducer,
-  user: userReducer
+  user: userReducer,
+  accessories: accessoryReducer
 });
