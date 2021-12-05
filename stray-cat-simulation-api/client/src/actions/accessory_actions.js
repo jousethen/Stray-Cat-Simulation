e0f4f630bb9d4eccc1708cc1d722ffbddb6c71e2
jousethen <jthen93@gmail.com>
2021-12-05 17:07:43 -0500

    Accessory_action.js added

---
+++ stray-cat-simulation-api/client/src/actions/accessory_actions.js
@@ -0,0 +1,34 @@
+export const fetchAccessories = () => {
+  return (dispatch) => {
+    dispatch({ type: "LOADING_ACCESSORIES" });
+    fetch('/api/accessories', {
+      method: 'GET', headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        let accessories = loadFreeAccessories(json)
+        dispatch({ type: "LOAD_ACCESSORIES_SUCCESS", accessories: accessories })
+      });
+  };
+}
+
+
+//*---------------UTILITY------------------------*//
+function loadFreeAccessories(accessories) {
+  let accArr = accessories.filter(a => {
+    return a.id === null
+  });
+  return accArr;
+}
+
+function loadtakenAccessories(accessories) {
+  let accArr = accessories.filter(a => {
+    return a.id !== null
+  });
+  return accArr;
+}
