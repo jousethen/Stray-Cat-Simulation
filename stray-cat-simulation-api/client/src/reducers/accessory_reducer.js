6dee3a982fb1f8d6218fab514cbb061276dd2ed6
jousethen <jthen93@gmail.com>
2021-12-07 14:33:20 -0500

    Added GIFT_ACCESSORY to accessory reducers

--- stray-cat-simulation-api/client/src/reducers/accessory_reducer.js
+++ stray-cat-simulation-api/client/src/reducers/accessory_reducer.js
@@ -9,6 +9,17 @@ export const accessoryReducer = (state = { accessories: [] }, action) => {
         ...state,
         accessories: action.accessories
       }
+    case "GIFT_ACCESSORY":
+      accIndex = state.accessories.findIndex((acc) => {
+        return acc.id === action.accId
+      });
+
+      state.accessories[accIndex].cat_id = action.catId
+
+      return {
+        ...state,
+        accessories: [...state.accessories],
+      }
     default:
       return state;
   }
