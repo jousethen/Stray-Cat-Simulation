af918b3b15315dc61ddd2c583e29fd1ddbed2383
jousethen <jthen93@gmail.com>
2021-12-07 21:48:41 -0500

    More changes from footer to header

--- stray-cat-simulation-api/client/src/components/Header.js
+++ stray-cat-simulation-api/client/src/components/Header.js
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap';
 const Footer = (props) => {
 
   return (
-    <div className="footer">
+    <div className="header">
       <h1>Actions: {props.actions} </h1>
 
       <Button
